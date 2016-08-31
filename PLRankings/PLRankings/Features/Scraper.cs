9ecbe48117f13475e60a0956fd811b382e84b99e
Eric Sondergard <flamtap@users.noreply.github.com>
2016-08-30 22:08:13 -0600

    basic html downloader
    Scraper doesn't actually "scrape" anything yet, just downloads the file

---
+++ PLRankings/PLRankings/Features/Scraper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace PLRankings.Features
+{
+    public class Scraper
+    {
+        private readonly string _outputFile;
+        private readonly Uri _resultsUri;
+
+        public Scraper(string resultsUri, string outputFile)
+        {
+            _outputFile = outputFile;
+            _resultsUri = new Uri(resultsUri);
+        }
+
+        public void GetData()
+        {
+            string result = GetRawHtml();
+
+            File.WriteAllText(_outputFile, result);
+        }
+
+        private string GetRawHtml()
+        {
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(_resultsUri);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding(response.CharacterSet)))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+
+            return "Error";
+        }
+    }
+}
--- PLRankings/PLRankings/Program.cs
+++ PLRankings/PLRankings/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PLRankings
+﻿namespace PLRankings
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            
         }
     }
 }
