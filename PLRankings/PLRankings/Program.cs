daad242a7b9f89aab72b253f4f6a82093a6c8619
Eric Sondergard <flamtap@users.noreply.github.com>
2016-08-30 22:26:54 -0600

    basic proof-of-concept
    added the page for 2015 open male raw lifters in AB to be run by default

--- PLRankings/PLRankings/Program.cs
+++ PLRankings/PLRankings/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace PLRankings
+﻿using PLRankings.Features;
+
+namespace PLRankings
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            
+            string openMenRaw2015 =
+                "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Open&BW%2FCl.+=&BW-CL-NEW=&Year=2015&NAME=&Uneq%3F=Y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+
+            string outputFile = "C:\\Users\\esond_000\\Desktop\\document.html";
+
+            Scraper scraper = new Scraper(openMenRaw2015, outputFile);
+            scraper.GetData();
         }
     }
 }
