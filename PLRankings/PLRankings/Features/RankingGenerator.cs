81d450833cc9e6bb784f7d62659d0e5df9f66e3c
Eric Sondergard <flamtap@users.noreply.github.com>
2016-09-27 21:58:27 -0600

    Adding ability to pass in optional filter for competition results to RankingGenerator
    This is required to be able to create an equipped competition ranking.

--- PLRankings/PLRankings/Features/RankingGenerator.cs
+++ PLRankings/PLRankings/Features/RankingGenerator.cs
@@ -10,7 +10,8 @@ namespace PLRankings.Features
 {
     public static class RankingGenerator
     {
-        public static void CreateRanking(string outputFile, IEnumerable<string> resultsUris)
+        public static void CreateRanking(string outputFile, IEnumerable<string> resultsUris,
+            Func<CompetitionResult, bool> additionalPredicate = null)
         {
             Console.WriteLine($"Creating ranking {outputFile}...");
 
@@ -18,7 +19,10 @@ namespace PLRankings.Features
 
             StringBuilder builder = new StringBuilder();
 
-            foreach (string line in SerializationHelper.ToCsv(results.OrderByDescending(r => r.WilksPoints)))
+            if (additionalPredicate == null)
+                additionalPredicate = x => true;
+
+            foreach (string line in SerializationHelper.ToCsv(results.Where(additionalPredicate).OrderByDescending(r => r.WilksPoints)))
             {
                 builder.AppendLine(line);
             }
