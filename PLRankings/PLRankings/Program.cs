0131497b1eda3ccd7029599db7fcb4cd2b8eb14d
Eric Sondergard <flamtap@users.noreply.github.com>
2016-12-18 19:58:41 -0700

    Create output directory if it doesn't exist.

--- PLRankings/PLRankings/Program.cs
+++ PLRankings/PLRankings/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Configuration;
 using System.IO;
-using System.Web.Configuration;
 using PLRankings.Data;
 using PLRankings.Features;
 
@@ -10,37 +9,38 @@ namespace PLRankings
     {
         public static void Main(string[] args)
         {
-            string outputFile = GetOutputFilePath("Men - Open (2016).csv");
+            string path = ConfigurationManager.AppSettings["outputFilePath"];
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            string outputFile = Path.Combine(path, "Men - Open (2016).csv");
+
             RankingGenerator.CreateRanking(outputFile, ResultsUris.OpenMen2016);
 
-            outputFile = GetOutputFilePath("Women - Open (2016).csv");
+            outputFile = Path.Combine(path, "Women - Open (2016).csv");
             RankingGenerator.CreateRanking(outputFile, ResultsUris.OpenWomen2016);
 
-            outputFile = GetOutputFilePath("Men - Junior and Sub-Junior (2016).csv");
+            outputFile = Path.Combine(path, "Men - Junior and Sub-Junior (2016).csv");
             RankingGenerator.CreateRanking(outputFile, ResultsUris.JuniorMen2016);
 
-            outputFile = GetOutputFilePath("Women - Junior and Sub-Junior (2016).csv");
+            outputFile = Path.Combine(path, "Women - Junior and Sub-Junior (2016).csv");
             RankingGenerator.CreateRanking(outputFile, ResultsUris.JuniorWomen2016);
 
-            outputFile = GetOutputFilePath("Men - Masters (2016).csv");
+            outputFile = Path.Combine(path, "Men - Masters (2016).csv");
             RankingGenerator.CreateRanking(outputFile, ResultsUris.MastersMen2016);
 
-            outputFile = GetOutputFilePath("Women - Masters (2016).csv");
+            outputFile = Path.Combine(path, "Women - Masters (2016).csv");
             RankingGenerator.CreateRanking(outputFile, ResultsUris.MastersWomen2016);
 
-            outputFile = GetOutputFilePath("Men - Bench-Only (2016).csv");
+            outputFile = Path.Combine(path, "Men - Bench-Only (2016).csv");
             RankingGenerator.CreateRanking(outputFile, ResultsUris.BenchOnlyMen2016);
 
-            outputFile = GetOutputFilePath("Women - Bench-Only (2016).csv");
+            outputFile = Path.Combine(path, "Women - Bench-Only (2016).csv");
             RankingGenerator.CreateRanking(outputFile, ResultsUris.BenchOnlyWomen2016);
 
-            outputFile = GetOutputFilePath("Overall - Equipped (2016).csv");
+            outputFile = Path.Combine(path, "Overall - Equipped (2016).csv");
             RankingGenerator.CreateRanking(outputFile, ResultsUris.EquippedOverall2016, cr => !cr.Unequipped);
-        }
-
-        private static string GetOutputFilePath(string fileName)
-        {
-            return Path.Combine(ConfigurationManager.AppSettings["outputFilePath"], fileName);
         }
     }
 }
