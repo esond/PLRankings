cb7db9de2867b156538801e14fd1c1153dc6a3ee
Eric Sondergard <flamtap@users.noreply.github.com>
2017-12-03 17:00:18 -0700

    Update results URL's to filter for 2017

--- PLRankings/PLRankings/Data/ResultsUris.cs
+++ PLRankings/PLRankings/Data/ResultsUris.cs
@@ -4,100 +4,100 @@ namespace PLRankings.Data
 {
     public static class ResultsUris
     {
-        public static IEnumerable<string> OpenMen2016
+        public static IEnumerable<string> OpenMen2017
         {
             get
             {
-                yield return //2016
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
             }
         }
 
-        public static IEnumerable<string> OpenWomen2016
+        public static IEnumerable<string> OpenWomen2017
         {
             get
             {
-                yield return //2016
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=&M+%2F+F=F&Prov.=AB&Age+Category=&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=&M+%2F+F=F&Prov.=AB&Age+Category=&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
             }
         }
 
-        public static IEnumerable<string> JuniorMen2016
+        public static IEnumerable<string> JuniorMen2017
         {
             get
             {
-                yield return //2016 Junior
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Junior&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
-                yield return //2016 Sub-Junior
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Sub-Junior&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 Junior
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Junior&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 Sub-Junior
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Sub-Junior&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
             }
         }
 
-        public static IEnumerable<string> JuniorWomen2016
+        public static IEnumerable<string> JuniorWomen2017
         {
             get
             {
-                yield return //2016 Junior
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=F&Prov.=AB&Age+Category=Junior&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
-                yield return //2016 Sub-Junior
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=F&Prov.=AB&Age+Category=Sub-Junior&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 Junior
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=F&Prov.=AB&Age+Category=Junior&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 Sub-Junior
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=F&Prov.=AB&Age+Category=Sub-Junior&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
             }
         }
 
-        public static IEnumerable<string> MastersMen2016
+        public static IEnumerable<string> MastersMen2017
         {
             get
             {
-                yield return //2016 M1
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Master+1&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
-                yield return //2016 M2
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Master+2&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
-                yield return //2016 M3
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Master+3&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
-                yield return //2016 M4
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Master+4&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 M1
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Master+1&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 M2
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Master+2&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 M3
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Master+3&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 M4
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=M&Prov.=AB&Age+Category=Master+4&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
             }
         }
 
-        public static IEnumerable<string> MastersWomen2016
+        public static IEnumerable<string> MastersWomen2017
         {
             get
             {
-                yield return //2016 M1
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=F&Prov.=AB&Age+Category=Master+1&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
-                yield return //2016 M2
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=F&Prov.=AB&Age+Category=Master+2&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
-                yield return //2016 M3
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=F&Prov.=AB&Age+Category=Master+3&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
-                yield return //2016 M4
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=F&Prov.=AB&Age+Category=Master+4&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 M1
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=F&Prov.=AB&Age+Category=Master+1&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 M2
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=F&Prov.=AB&Age+Category=Master+2&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 M3
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=F&Prov.=AB&Age+Category=Master+3&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 M4
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=F&Prov.=AB&Age+Category=Master+4&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
             }
         }
 
-        public static IEnumerable<string> BenchOnlyMen2016
+        public static IEnumerable<string> BenchOnlyMen2017
         {
             get
             {
-                yield return //2016 Open
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=Single&M+%2F+F=M&Prov.=AB&Age+Category=&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 Open
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=Single&M+%2F+F=M&Prov.=AB&Age+Category=&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
             }
         }
 
-        public static IEnumerable<string> BenchOnlyWomen2016
+        public static IEnumerable<string> BenchOnlyWomen2017
         {
             get
             {
-                yield return //2016 open
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=Single&M+%2F+F=F&Prov.=AB&Age+Category=&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
+                yield return //2017 open
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=Single&M+%2F+F=F&Prov.=AB&Age+Category=&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=y&Contest=&cgifunction=Search&pagenum=1&cgisort=17&cgisortorder=1";
             }
         }
 
-        public static IEnumerable<string> EquippedOverall2016
+        public static IEnumerable<string> EquippedOverall2017
         {
             get
             {
                 yield return
-                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=&Prov.=AB&Age+Category=&BW%2FCl.+=&BW-CL-NEW=&Year=2016&NAME=&Uneq%3F=&Contest=&cgifunction=Search&pagenum=1&cgisort=12&cgisortorder=0";
+                    "http://www.powerlifting.ca/cgi-bin/webdata_cpudb.pl?3+Lift%2FSingle=All&M+%2F+F=&Prov.=AB&Age+Category=&BW%2FCl.+=&BW-CL-NEW=&Year=2017&NAME=&Uneq%3F=&Contest=&cgifunction=Search&pagenum=1&cgisort=12&cgisortorder=0";
             }
         }
     }
--- PLRankings/PLRankings/Program.cs
+++ PLRankings/PLRankings/Program.cs
@@ -14,33 +14,33 @@ namespace PLRankings
             if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
 
-            var outputFile = Path.Combine(path, "Men - Open (2016).csv");
+            var outputFile = Path.Combine(path, "Men - Open (2017).csv");
 
-            RankingGenerator.CreateRanking(outputFile, ResultsUris.OpenMen2016);
+            RankingGenerator.CreateRanking(outputFile, ResultsUris.OpenMen2017);
 
-            outputFile = Path.Combine(path, "Women - Open (2016).csv");
-            RankingGenerator.CreateRanking(outputFile, ResultsUris.OpenWomen2016);
+            outputFile = Path.Combine(path, "Women - Open (2017).csv");
+            RankingGenerator.CreateRanking(outputFile, ResultsUris.OpenWomen2017);
 
-            outputFile = Path.Combine(path, "Men - Junior and Sub-Junior (2016).csv");
-            RankingGenerator.CreateRanking(outputFile, ResultsUris.JuniorMen2016);
+            outputFile = Path.Combine(path, "Men - Junior and Sub-Junior (2017).csv");
+            RankingGenerator.CreateRanking(outputFile, ResultsUris.JuniorMen2017);
 
-            outputFile = Path.Combine(path, "Women - Junior and Sub-Junior (2016).csv");
-            RankingGenerator.CreateRanking(outputFile, ResultsUris.JuniorWomen2016);
+            outputFile = Path.Combine(path, "Women - Junior and Sub-Junior (2017).csv");
+            RankingGenerator.CreateRanking(outputFile, ResultsUris.JuniorWomen2017);
 
-            outputFile = Path.Combine(path, "Men - Masters (2016).csv");
-            RankingGenerator.CreateRanking(outputFile, ResultsUris.MastersMen2016);
+            outputFile = Path.Combine(path, "Men - Masters (2017).csv");
+            RankingGenerator.CreateRanking(outputFile, ResultsUris.MastersMen2017);
 
-            outputFile = Path.Combine(path, "Women - Masters (2016).csv");
-            RankingGenerator.CreateRanking(outputFile, ResultsUris.MastersWomen2016);
+            outputFile = Path.Combine(path, "Women - Masters (2017).csv");
+            RankingGenerator.CreateRanking(outputFile, ResultsUris.MastersWomen2017);
 
-            outputFile = Path.Combine(path, "Men - Bench-Only (2016).csv");
-            RankingGenerator.CreateRanking(outputFile, ResultsUris.BenchOnlyMen2016);
+            outputFile = Path.Combine(path, "Men - Bench-Only (2017).csv");
+            RankingGenerator.CreateRanking(outputFile, ResultsUris.BenchOnlyMen2017);
 
-            outputFile = Path.Combine(path, "Women - Bench-Only (2016).csv");
-            RankingGenerator.CreateRanking(outputFile, ResultsUris.BenchOnlyWomen2016);
+            outputFile = Path.Combine(path, "Women - Bench-Only (2017).csv");
+            RankingGenerator.CreateRanking(outputFile, ResultsUris.BenchOnlyWomen2017);
 
-            outputFile = Path.Combine(path, "Overall - Equipped (2016).csv");
-            RankingGenerator.CreateRanking(outputFile, ResultsUris.EquippedOverall2016, cr => !cr.Unequipped);
+            outputFile = Path.Combine(path, "Overall - Equipped (2017).csv");
+            RankingGenerator.CreateRanking(outputFile, ResultsUris.EquippedOverall2017, cr => !cr.Unequipped);
         }
     }
 }
